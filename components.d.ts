5c3aeb07e7cca08df82ee6aaaf218d4ebc0a689f
荣哥 <15035291867@163.com>
2022-01-25 16:04:10 +0800

    feat: 添加基本的路由和store

--- components.d.ts
+++ components.d.ts
@@ -4,7 +4,7 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    // HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
   }
 }
 
