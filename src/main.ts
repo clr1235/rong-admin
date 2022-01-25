a95f4ed9a3013f7cd042d8919d666a2935b075c7
荣哥 <15035291867@163.com>
2022-01-25 17:25:06 +0800

    feat: 全局注册一些常用的ui组件

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,18 @@ import 'makeit-captcha/dist/captcha.min.css';
 import './styles/tailwind.css';
 import { setupStore } from './store';
 import router, { setupRouter } from './router';
+import { setupNaive } from '@/plugins';
+
 import App from './App.vue';
 async function init() {
   const app = createApp(App);
   // 全局注册滑块组件
   app.use(MakeitCaptcha);
+  // 全局注册一些常用的ui组件
+  setupNaive(app);
+  // 全局注册自定义组件
+
+  // 全局注册自定义指令
 
   // 注册全局的store
   setupStore(app);
